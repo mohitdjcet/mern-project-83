1fb517c682973338b809dfe2c252292f1d4c70f4
Mohit Kumar <mks.djcet@gmail.com>
2026-03-07 15:26:31 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 function App() {
-  const age = 29
-  return (
+  const [text, setText] = useState("");
+
+  const handleFocus = () => {
+    console.log("Input field is focused");
+  }
+  const handleBlur = () => {
+    console.log("Input field is blurred");
+  }
+  return(
     <>
-      <h1 class="header">Hello React {1 + 1}</h1>
+      <input 
+      type="text" 
+      value={text}
+      onChange={(e)=> setText(e.target.value)}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      placeholder="Enter your name" />
+      <h2>You Type: {text}</h2>
     </>
-  );
+  )
 }
 
 export default App;
