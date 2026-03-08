b74b4405ef67aea9cdc4ba89e1a21a6c8da1d9f0
Mohit Kumar <mks.djcet@gmail.com>
2026-03-08 13:50:17 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+// import { useState } from "react";
+import { useRef } from "react";
+
 function App() {
-  const fruits = [];
+  // const [name, setName] = useState("Mohit");
+  
+  const inputRef = useRef(null);
+
+  // const handleSubmit = (e)=>{
+  //   e.preventDefault();
+  //   console.log("Name",name);
+  // }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log("Name", inputRef.current.value);
+  }
   return (
-    <div>
-      <ul>
-        {loading && (
-          fruits.map((fruit, index) => <li key={fruit.id}>{fruit.name}</li>)
-        )}
-      </ul>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        // value={name}
+        // onChange={(e) => setName(e.target.value)}
+        ref = {inputRef}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 export default App;
