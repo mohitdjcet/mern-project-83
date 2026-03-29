21469f6ea7852ea0f8113687576d50c972b7e89c
Mohit Kumar <mks.djcet@gmail.com>
2026-03-29 12:31:50 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  const age = 29
+  const [users,setUsers] = useState([]);
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+        console.log(res);
+        setUsers(res.data);
+      } catch (err) {
+        setError("Something went wrong")
+      } finally{
+        setLoading(false)
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p style={{color:"red"}}>{error}</p>
   return (
-    <>
-      <h1 class="header">Hello React {1 + 1}</h1>
-    </>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
