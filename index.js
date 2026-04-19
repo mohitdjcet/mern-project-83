637417abe30cab8f666360b5f0275f3487347853
Mohit Kumar <mks.djcet@gmail.com>
2026-04-19 18:38:01 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-// import fs from "fs/promises";
-const fs = require("fs")
+const fs = require("fs").promises;
 
-
-// fs.writeFile("data.txt","Mohit Kumar",(err)=>{
-//     if(err){
-//         console.log("Error");
-//         return;
-//     }
-//     console.log("File Created successfully");
-// })
-
-// fs.appendFile("data.txt","\nNew Line Added")
-// console.log("Data Append is working fine")
-
-fs.unlink("data.txt",(err)=>{
-    if(err){
-        console.log("File not found");
-        return;
+async function fileOps() {
+    try{
+        await fs.writeFile("demo.txt","Hello World");
+        await fs.appendFile("demo.txt","\nMohit");
+        await fs.unlink("demo.txt");
+        console.log("All opr completed");
+    } catch (err){
+        console.log("Error", err); 
     }
-    console.log("File deleted successfully");
-})
+}
+fileOps()
