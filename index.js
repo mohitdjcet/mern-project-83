37eb558cbfbfa5439c55749c4a2daecacc721dbf
Mohit Kumar <mks.djcet@gmail.com>
2026-04-25 18:06:23 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-//HTTP Module
-// const http = require("http");
-import http from "http";
+import chalk from "chalk";
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{
-        "content-type":"text/HTML"
-    })
-    res.end(`<h1>Mohit Kumar</h1>`);
-});
+console.log(chalk.blue("Hello Mohit"));
 
-server.listen(3005,()=>{
-    console.log("Server is running on port 3005");
-})
+console.log(chalk.red("Hello Mohit"));
