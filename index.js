2ff25ed4efd18c68187342a12a1e29004eefd7d6
Mohit Kumar <mks.djcet@gmail.com>
2026-04-25 18:28:30 +0530

    Code done

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-import chalk from "chalk";
+// import fs from "fs"
 
-console.log(chalk.blue("Hello Mohit"));
+// const data = fs.readFileSync("test.txt","utf8");
+// console.log(data);
+// console.log("Progream end");
 
-console.log(chalk.red("Hello Mohit"));
+// console.log("Tsk-1");
+// setTimeout(()=>{
+//     console.log("Tsk-2");
+// },2000)
+
+// console.log("Tast-3");
+
+
+// function greet(name, callback){
+//     console.log("Hello "+ name);
+//     callback()
+// }
+// function sayBye(){
+//     console.log("Bye");
+// }
+// greet("Mohit",sayBye)
+
+function loginUser(callback){
+    setTimeout(()=>{
+        console.log("User Logged In");
+        callback()
+    },1000)
+}
+function getProfile(callback){
+    setTimeout(()=>{
+        console.log("Profile Loaded");
+        callback()
+    },1000)
+}
+function getPost(callback){
+    setTimeout(()=>{
+        console.log("Posts Loaded");
+        callback()
+    },1000)
+}
+
+loginUser(()=>{
+    getProfile(()=>{
+        getPost(()=>{
+            console.log("All Task Done");
+        })
+    })
+})
