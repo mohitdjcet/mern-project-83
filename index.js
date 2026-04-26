590e7828275254643c946fc1da4fc1888f85f500
Mohit Kumar <mks.djcet@gmail.com>
2026-04-26 19:46:31 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
-// import fs from "fs"
+function loginUser() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("User Logged In");
+            resolve();
+        }, 1000);
+    });
+}
 
-// const data = fs.readFileSync("test.txt","utf8");
-// console.log(data);
-// console.log("Progream end");
+function getProfile() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Profile Loaded");
+            resolve();
+        }, 1000);
+    });
+}
 
-// console.log("Tsk-1");
-// setTimeout(()=>{
-//     console.log("Tsk-2");
-// },2000)
+function getPost() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Posts Loaded");
+            resolve();
+        }, 1000);
+    });
+}
 
-// console.log("Tast-3");
+async function runApp() {
+    await loginUser();
+    await getProfile();
+    await getPost();
+    console.log("All Task Done");
+}
 
+runApp();
+// loginUser()
+//     .then(() => getProfile())
+//     .then(() => getPost())
+//     .then(() => {
+//         console.log("All Task Done");
+//     });
 
-// function greet(name, callback){
-//     console.log("Hello "+ name);
-//     callback()
+// loginUser(()=>{
+//     getProfile(()=>{
+//         getPost(()=>{
+//             console.log("All Task Done");
+//         })
+//     })
+// })
+
+// function step1(){
+//     return Promise.resolve("Step 1 done")
 // }
-// function sayBye(){
-//     console.log("Bye");
+// function step2(){
+//     return Promise.resolve("Step 2 done")
 // }
-// greet("Mohit",sayBye)
-
-function loginUser(callback){
-    setTimeout(()=>{
-        console.log("User Logged In");
-        callback()
-    },1000)
-}
-function getProfile(callback){
-    setTimeout(()=>{
-        console.log("Profile Loaded");
-        callback()
-    },1000)
-}
-function getPost(callback){
-    setTimeout(()=>{
-        console.log("Posts Loaded");
-        callback()
-    },1000)
-}
-
-loginUser(()=>{
-    getProfile(()=>{
-        getPost(()=>{
-            console.log("All Task Done");
-        })
-    })
-})
+// function step3(){
+//     return Promise.resolve("Step 3 done")
+// }
+// step1()
+//     .then((result)=>{
+//         console.log(result);
+//         return step2();
+//     }).then((result)=>{
+//         console.log(result);
+//         return step3();
+//     }).then((result)=>{
+//         console.log(result);
+//     })
