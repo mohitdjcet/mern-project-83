a8836c620739de1f857b119c8e19ff3b434b17b7
Mohit Kumar <mks.djcet@gmail.com>
2026-04-26 19:55:52 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,69 +1,11 @@
-function loginUser() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("User Logged In");
-            resolve();
-        }, 1000);
-    });
+import axios from "axios"
+
+async function getUsers(){
+    try{
+        const responce = await axios.get("https://jsonplaceholder.typicode.com/users");
+        console.log(responce.data);
+    } catch(error){
+        console.log(error);
+    }
 }
-
-function getProfile() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Profile Loaded");
-            resolve();
-        }, 1000);
-    });
-}
-
-function getPost() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log("Posts Loaded");
-            resolve();
-        }, 1000);
-    });
-}
-
-async function runApp() {
-    await loginUser();
-    await getProfile();
-    await getPost();
-    console.log("All Task Done");
-}
-
-runApp();
-// loginUser()
-//     .then(() => getProfile())
-//     .then(() => getPost())
-//     .then(() => {
-//         console.log("All Task Done");
-//     });
-
-// loginUser(()=>{
-//     getProfile(()=>{
-//         getPost(()=>{
-//             console.log("All Task Done");
-//         })
-//     })
-// })
-
-// function step1(){
-//     return Promise.resolve("Step 1 done")
-// }
-// function step2(){
-//     return Promise.resolve("Step 2 done")
-// }
-// function step3(){
-//     return Promise.resolve("Step 3 done")
-// }
-// step1()
-//     .then((result)=>{
-//         console.log(result);
-//         return step2();
-//     }).then((result)=>{
-//         console.log(result);
-//         return step3();
-//     }).then((result)=>{
-//         console.log(result);
-//     })
+getUsers()
