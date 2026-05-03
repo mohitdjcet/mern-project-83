e4816d8d08bc6766102b6fbe15318fc918b473e9
Mohit Kumar <mks.djcet@gmail.com>
2026-05-03 22:47:47 +0530

    Updated code

--- server.js
+++ server.js
@@ -1,37 +1,65 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Welcome to Home Page")
-})
+let users = [
+  {
+    id: 12345,
+    name: "Mohit",
+  },
+  {
+    id: 12346,
+    name: "Vipin",
+  },
+];
 
-app.get("/users", (req,res)=>{
-    console.log("Users API hit");
-    const users = {
-        "name" :"Mohit",
-        "age":24
-    };
-    res.json(users)
-})
+app.get("/users", (req, res) => {
+  res.json({
+    data: users,
+  });
+});
 
-app.get("/user/search",(req,res)=>{
-    const price = req.query.price;
+app.put("/update-user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const newName = req.body.name;
+  console.log(userId);
 
-    res.send(`Search for ${price}`)
-})
+  const user = users.find((u) => u.id === userId);
 
-app.post("/add-user",(req,res)=>{
-    const user = req.body;
+  if (user) {
+    user.name = newName;
+    res.json({
+      message: "User Upadated fine",
+      users,
+    });
+  } else {
+    res.status(404).json({
+      message: "User Not Found",
+    });
+  }
+});
+
+app.delete("/delete-user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+
+  const newUsers = users.filter((u) => u.id !== userId);
+
+  if (newUsers.length !== users.length) {
+    users = newUsers;
 
     res.json({
-        message: "User Added Fine",
-        user: user
-    })
-})
+      message: "User Deleted",
+      users,
+    });
+  } else {
+    res.status(404).json({
+      message: "User Not Found",
+    });
+  }
+});
 
-app.listen(3005,()=>{
-    console.log("Server is Running on port 3005");
-})
+app.listen(3005, () => {
+  console.log("Server is Running on port 3005");
+});
